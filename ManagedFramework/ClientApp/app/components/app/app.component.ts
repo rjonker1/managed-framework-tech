7c86809db41ee2ade3a6dd6c51f0368a38cfa4cc
Rudi Jonker <Rudi Jonker>
2017-03-18 14:44:00 +0200

    hide menu on login

--- ManagedFramework/ClientApp/app/components/app/app.component.ts
+++ ManagedFramework/ClientApp/app/components/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+import { User } from '../../_models/index';
+import { UserService } from '../../_services/index';
+
 @Component({
     moduleId: module.id.toString(),
     selector: 'app',
@@ -7,4 +10,14 @@ import { Component } from '@angular/core';
     styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+    currentUser: User;
+
+    constructor(private userService: UserService) {
+        this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
+    }
+
+    showNavMenu() {
+        return this.currentUser != null;
+    }
 }
--- ManagedFramework/ClientApp/app/components/navmenu/navmenu.component.ts
+++ ManagedFramework/ClientApp/app/components/navmenu/navmenu.component.ts
@@ -1,9 +1,36 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { User } from '../../_models/index';
+import { UserService } from '../../_services/index';
 
 @Component({
+    moduleId: module.id.toString(),
     selector: 'nav-menu',
     templateUrl: './navmenu.component.html',
     styleUrls: ['./navmenu.component.css']
 })
-export class NavMenuComponent {
+export class NavMenuComponent  {
+
+    private currentUser: User;
+
+    constructor(private userService: UserService, private _router: Router) {
+        
+        this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
+    }
+
+    //checkCredentials() {
+    //    if (localStorage.getItem("currentUser") === null) {
+    //        this._router.navigate(['Login']);
+    //    }
+    //}
+
+    showNavMenu() {       
+        if (this.currentUser != null)
+        {
+            return true;
+        }
+
+        this._router.navigate(['Login']);
+    }
 }
